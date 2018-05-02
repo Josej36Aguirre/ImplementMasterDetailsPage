9e6516918272e83d2a676dade4d8b79370adc997
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-05-02 01:31:50 -0500

    Initial commit

---
+++ DemoNavigationPage/DemoNavigationPage/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DemoNavigationPage
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new DemoNavigationPage.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
