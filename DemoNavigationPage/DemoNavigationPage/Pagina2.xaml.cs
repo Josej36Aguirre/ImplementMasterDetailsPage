01ef6733eaf2b6c1322acbe00e98eef73dd53f60
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-05-02 04:51:51 -0500

    se Agrego 4 paginaspara navegar entre ellas

--- DemoNavigationPage/DemoNavigationPage/Pagina2.xaml.cs
+++ DemoNavigationPage/DemoNavigationPage/Pagina2.xaml.cs
@@ -15,6 +15,34 @@ namespace DemoNavigationPage
 		public Pagina2 ()
 		{
 			InitializeComponent ();
+            this.lbOpcion.Text = string.Empty;
+            btnTres.Clicked += BtnTres_Clicked;
+            btnTest.Clicked += BtnTest_Clicked;
 		}
-	}
+
+        private async void BtnTest_Clicked(object sender, EventArgs e)
+        {
+            this.lbOpcion.Text = string.Empty;
+            var resp = await DisplayAlert("Test", "Esta seguro de realizar el test", "Si", "No");
+            if (resp)
+            {
+                await DisplayAlert("Inicio de test", "Super Bienvenido", "Aceptar");
+                var opcion = await DisplayActionSheet("Opciones",
+                                                        "Cancelar",
+                                                        null, "Opcion 1",
+                                                        "Opcion 2", "Opcion 3");
+                lbOpcion.Text = opcion.ToString();
+            }
+            else
+            {
+                await DisplayAlert("Test Fallido", "En otra Ocación Sera", "Aceptar");
+            }
+        }
+
+        private async void BtnTres_Clicked(object sender, EventArgs e)
+        {
+            this.lbOpcion.Text = string.Empty;
+            await Navigation.PushAsync(new Page3());
+        }
+    }
 }
